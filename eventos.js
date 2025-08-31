ee644718b512b4ec34df1cd5098e64507211832b
Celina Leicht <celileicht23@gmail.com>
2025-08-31 13:05:31 -0300

    stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (event) {
     const boton = document.getElementById("btnSaludar");
     boton.addEventListener("click", function () {
+        event.stopPropagation();
         alert("Hola!");
 
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(event) {
     const div = document.getElementById("section");
     section.addEventListener("click", function(){
+        event.stopPropagation();
         alert("Hola! Soy el div");
     })
 })
